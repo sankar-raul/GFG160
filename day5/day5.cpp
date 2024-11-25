7ae9108d9c493bc4351ada2278ebb1cfcbd68783
sankar-raul <raulsankar99@gmail.com>
2024-11-25 18:11:26 +0530

    day5 cpp

---
+++ day5/day5.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+  public:
+    void nextPermutation(vector<int>& arr) {
+        int pivot = -1;
+        int n = arr.size(), i = 0;
+        for (i = n - 2; i >= 0; i--) {
+            if (arr[i] < arr[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+        if (pivot == -1) {
+            reverse(arr, 0, n - 1);
+            return;
+        }
+        for (i = n - 1; i > pivot; i--) {
+            if (arr[i] > arr[pivot]) {
+                int temp = arr[i];
+                arr[i] = arr[pivot];
+                arr[pivot] = temp;
+                break;
+            }
+        }
+        reverse(arr, pivot + 1, n - 1);
+    }
+    void reverse(vector<int> &arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start++] = arr[end];
+            arr[end--] = temp;
+        }
+    }
+};
+int main() {
+    vector<int> arr = {2, 4, 1, 7, 5, 0};
+    int n = arr.size();
+    Solution Answer = Solution();
+    for (int i = 0; i < n; i++) {
+        cout << ' ' << arr[i] << ",";
+    }
+    cout << endl;
+    Answer.nextPermutation(arr);
+    for (int i = 0; i < n; i++) {
+        cout << ' ' << arr[i] << ",";
+    }
+    cout << endl;
+    return 0;
+}
